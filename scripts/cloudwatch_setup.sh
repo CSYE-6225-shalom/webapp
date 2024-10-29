14c2407ed52ccba40e57ee1638a5a9135261f48a
Shalom Daniel <shalomd498@gmail.com>
2024-10-28 22:42:24 -0400

    cloudwatch configs, setup and scripts

---
+++ scripts/cloudwatch_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -O https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+sudo systemctl enable amazon-cloudwatch-agent
+sudo systemctl start amazon-cloudwatch-agent
